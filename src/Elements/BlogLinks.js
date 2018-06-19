a18f32bf830a5a672ad0c041f10692e1435fcb74
Alex <alex@wendtedesigns.com>
2018-06-19 12:36:23 -0500

    Adds some responsive styling to the competitions page

--- src/Elements/BlogLinks.js
+++ src/Elements/BlogLinks.js
@@ -7,7 +7,7 @@ import { BlogLink } from './';
 // defaultImage is the default image for the given blog type
 const BlogLinks = ({ pages, category, defaultImage }) => (
   // returns the list of li's encapsulated in a ul
-  <ul className="page-links">
+  <ul className="blog-links">
     {/* Once I learn about it, I will use the context api to pass category and default image right into BlogLink */}
     {map(pages, page => <BlogLink page={page} category={category} defaultImage={defaultImage} key={page.slug} />)}
   </ul>
