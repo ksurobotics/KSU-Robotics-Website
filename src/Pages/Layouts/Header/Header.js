cc72a4bfc7c6481d52c6491773850189f98b8bb1
Alex <alex@wendtedesigns.com>
2018-06-19 21:05:08 -0500

    Removes the border bottom on header from responsive competitions page. Adds some better respompetitions page

--- src/Pages/Layouts/Header/Header.js
+++ src/Pages/Layouts/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -9,45 +9,36 @@ import purpleLogo from 'assets/images/Purple-Logo.png';
 // The Header has urls that have an active state and an inactive state.
 // The Contact us button does not display on the home page because there
 // Is already a contact us button on the home page
-class Header extends Component {
-  render() {
-    // This will take away the active-url's underline. I believe I should actually just do this logic myslef...
-    const hover = () => {
-      // console.log(this.className);
-      // console.log(this);
-    };
-    return (
-      <div className={`header-wrapper ${this.props.location === '/' ? 'home' : 'general'}`}>
-        <NavLink className="logo" to="/">
-          <img src={this.props.location === '/' ? logo : purpleLogo} alt="KSU Robotics Logo" />
+const Header = ({ location }) => (
+  <div className={`header-wrapper ${location === '/' ? 'home' : 'general'}`}>
+    <NavLink className="logo" to="/">
+      <img src={location === '/' ? logo : purpleLogo} alt="KSU Robotics Logo" />
+    </NavLink>
+    <ul className={`main-nav ${location.toLowerCase() === '/competitions' ? 'active-competitions' : ''}`}>
+      <li>
+        <NavLink exact activeClassName="current-url" to="/">
+          Home
         </NavLink>
-        <ul className="main-nav">
-          <li>
-            <NavLink exact activeClassName="current-url" to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="current-url" onMouseOver={hover} onFocus={hover} to="/About-Us">
-              About Us
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="current-url" to="/Competitions">
-              Competitions
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="current-url" to="/Robots">
-              Robots
-            </NavLink>
-          </li>
-        </ul>
-        <Button className={`primary ${this.props.location === '/' ? 'hidden' : ''}`}>Contact Us!</Button>
-      </div>
-    );
-  }
-}
+      </li>
+      <li>
+        <NavLink activeClassName="current-url" to="/About-Us">
+          About Us
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="current-url" to="/Competitions">
+          Competitions
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="current-url" to="/Robots">
+          Robots
+        </NavLink>
+      </li>
+    </ul>
+    <Button className={`primary ${location === '/' ? 'hidden' : ''}`}>Contact Us!</Button>
+  </div>
+);
 
 Header.propTypes = {
   location: PropTypes.string.isRequired,
